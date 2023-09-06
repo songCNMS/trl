c03db3d8bdfce0dce7e1894c05d49557b987ad37
Lei Song <lesong@microsoft.com>
2023-09-06 14:26:23 +0000

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+source /app/azureuser/anaconda3/etc/profile.d/conda.sh
+export DEFAULT_XDG_CACHE_HOME=/app/azureuser/.cache/
+export HF_HOME=/app/azureuser/.cache/huggingface
+export HF_DATASETS_CACHE=/app/azureuser/.cache/huggingface/datasets
+export TRANSFORMERS_CACHE=/app/azureuser/.cache/huggingface/models
