536f11cf0e9e741336a90f2d72b8178d5ac7652b
SL <lei.song@microsoft.com>
2024-10-16 14:51:02 +0000

    u

--- examples/scripts/sft.py
+++ examples/scripts/sft.py
@@ -46,7 +46,11 @@ python examples/scripts/sft.py \
     --lora_r=64 \
     --lora_alpha=16 
 """
+
+
+# python examples/scripts/sft.py     --model_name_or_path="Qwen/Qwen2.5-3B-Instruct"     --dataset_text_field="text"     --report_to="wandb"     --learning_rate=1.41e-5     --per_device_train_batch_size=4     --gradient_accumulation_steps=2     --output_dir="logs/qwen2.5-3B-sft-Instruct"     --logging_steps=100     --save_steps=1000     --num_train_epochs=100     --max_steps=-1     --gradient_checkpointing     --use_peft     --lora_r=64     --lora_alpha=16 
 import os
+
 from pathlib import Path
 path = Path(os.path.dirname(os.path.realpath(__file__)))
 import sys
