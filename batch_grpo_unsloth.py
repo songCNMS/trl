4d1a8732bbfb59305d7530c7ad3bef7736c63af0
Lei Song <you@example.com>
2025-02-24 02:12:16 +0000

    u

---
+++ batch_grpo_unsloth.py
@@ -0,0 +1,18 @@
+import os
+
+
+node_name = os.uname().nodename
+
+for base_model in [
+    "unsloth/Meta-Llama-3.1-8B-Instruct", 
+    "unsloth/Qwen2.5-14B-Instruct", 
+    "unsloth/phi-4"
+]:
+    read_name = base_model.replace("/", "_")
+    prefix = base_model.split("/")[0]
+    suffix = base_model.split("/")[1]
+    rs = [16, 32]
+    alphas = [16, 32]
+    for r in rs:
+        for alpha in alphas:
+            os.system(f"python grpo_unsloth.py base_model={base_model} r={r} alpha={alpha}")
